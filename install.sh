04fc0f9309a5c32f718cf5a61de6699cafa95f8f
DrMint <thomas.barillot@etu.u-bordeaux.fr>
2022-01-06 22:02:02 +0100

    Added cwebp to convert thumbnails

--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
 wget https://github.com/yt-dlp/yt-dlp/releases/download/2021.12.27/yt-dlp
 chmod +x yt-dlp
+
 wget https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz
 tar -xf *.tar.xz
 mv ffmpeg-*/ffmpeg .
 mv ffmpeg-*/ffprobe .
 rm -r ffmpeg-*/
-rm *.tar.xz
+
+wget https://storage.googleapis.com/downloads.webmproject.org/releases/webp/libwebp-1.2.2-rc1-linux-x86-64.tar.gz
+tar -xf *.tar.xz
+mv libwebp-*/bin/cwebp .
+rm -r libwebp-*
