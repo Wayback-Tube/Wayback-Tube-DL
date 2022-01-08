30c4948a9cf3ac121fdb1544b97f7dad90029d97
DrMint <thomas.barillot@etu.u-bordeaux.fr>
2022-01-08 11:09:34 +0100

    Fixed bug with the wrong namefile for removal

--- install.sh
+++ install.sh
@@ -5,9 +5,9 @@ wget https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.x
 tar -xf *.tar.xz
 mv ffmpeg-*/ffmpeg .
 mv ffmpeg-*/ffprobe .
-rm -r ffmpeg-*/
+rm -r ffmpeg-*
 
 wget https://storage.googleapis.com/downloads.webmproject.org/releases/webp/libwebp-1.2.2-rc1-linux-x86-64.tar.gz
-tar -xf *.tar.xz
+tar -xf *.tar.gz
 mv libwebp-*/bin/cwebp .
 rm -r libwebp-*
